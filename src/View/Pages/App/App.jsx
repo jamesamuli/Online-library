8df6c2d8266bb7304e846e763098daa7c687a9cc
jamesamuli <jamesamuli1998@gmail.com>
2022-10-16 02:53:20 +0300

    Finishing designing the home page

--- src/View/Pages/App/App.jsx
+++ src/View/Pages/App/App.jsx
@@ -2,15 +2,17 @@ import './App.css';
 import react from 'react';
 import { Route, Routes } from 'react-router';
 import Dashboard from '../Dashboard/Dashboard';
-import Sidebar from '../../Components/Sidebar/Sidebar';
+// import Sidebar from '../../Components/Sidebar/Sidebar';
+import Home from '../Home/Home';
 
 export default class App extends react.Component{
   render (){
     return(
       <div className='App'>
-        <Sidebar/>
+        {/* <Sidebar/> */}
         <Routes>
-            <Route index element = {<Dashboard/>}></Route>
+            <Route index element = {<Home/>}></Route>
+            <Route path='/dashboard' element = {<Dashboard/>} />
             <Route path='*' element = {<div>Page not found</div>}/>
         </Routes>
       </div>
